60c18e5d47d013fb2089abfd62ab139e023f316f
paulsvh <pvonhagen26@gmail.com>
2020-08-17 21:56:50 -0700

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchDogPics();
+})
+
+function fetchDogPics(){
+    const dogPicsUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(dogPicsUrl)
+    .then(response => response.json())
+    .then(json => {
+        json.message.forEach(image => renderDogs(image))
+    });
+}
+
+function renderDogs(dogPicsUrl){
+    const images = document.getElementById("dog-image-container");
+    let newPic = document.createElement('img');
+    newPic.src = dogPicsUrl;
+    images.appendChild(newPic);
+}
